a22d0e4b60a7df30ad352037fbc382049b76ce10
Wooncloud <wooncloud@hotmail.com>
2024-02-15 09:27:52 +0900

    홈화면 애드센스 제거

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,16 +32,6 @@
 			도시부엉 리틀리<br>(dosiowl littly)
 		</a>
 	</div>
-	<!-- wooncloud ad 01 -->
-	<ins class="adsbygoogle"
-		style="display:block"
-		data-ad-client="ca-pub-1003422907558705"
-		data-ad-slot="2923922799"
-		data-ad-format="auto"
-		data-full-width-responsive="true"></ins>
-	<script>
-		(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
 </div>
 
 <style>
